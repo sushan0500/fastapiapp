84fa944de7a3ae4f542fbf5601ef7c12443900fe
sushan0500 <4al23ai058@aiet.org.in>
2026-06-29 14:48:36 +0530

    testgit

---
+++ frontend/test.js
@@ -0,0 +1,52 @@
+// console.log("Hello, World!");
+// let x =10;
+// console.log(x);
+// const y = 20;
+// console.log(y);
+// console.log(x + y);
+// function add(X, Y) {
+//     return X + Y;
+// }
+// console.log(add(10, 20));
+// const arr = [10, 20, 30, 40, 50];
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+
+// dictinonary
+// const dict = {"name": "John", "age": 30, "city": "New York"};
+// console.log(dict["name"]);
+// console.log(dict["age"]);
+// console.log(dict["city"]);
+
+// // destructuring
+// const arr = [10, 20, 30, 40, 50];
+// let [a, b, c, d, e] = arr;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+
+// rest and spread
+// spread operator -->...
+// let a= [10, 20];
+// let b = [30, 40];
+// console.log(...a, ...b);
+// console.log([...a, ...b]);
+
+//rest operator -->...(variable name)
+
+// function add(a, ...b){
+//     return a + b;
+// }
+// console.log(add(10, 20, 30, 40, 50));
+// console.log(add(10, 20));
+// console.log(add(10, 20, 30));
+
+//Template literals
+// const a = 10;
+// const b = 20;
+// console.log(`The sum of ${a} and ${b} is ${a + b}`);
