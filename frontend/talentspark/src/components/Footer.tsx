dcf49c7530fc3eb7dadbf3d67bd7e3b1769416c1
sushan0500 <4al23ai058@aiet.org.in>
2026-06-29 16:45:28 +0530

    testgit

---
+++ frontend/talentspark/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <p>All rights reserved by Talentspark</p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ frontend/talentspark/src/components/JobCard.tsx
@@ -0,0 +1,12 @@
+function JobCard() {
+    return(
+        <div>
+            <h1>Software Engineering</h1>
+            <p>Google</p>
+            <p>Bnaglore</p>
+            <p>5 LPA</p>
+        </div>
+    )
+}
+
+export default JobCard
