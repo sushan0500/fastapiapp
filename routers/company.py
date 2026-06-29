8388ceecac3b677a76b39e79186e8f6291c126d2
sushan0500 <4al23ai058@aiet.org.in>
2026-06-29 09:50:55 +0530

    testgit

--- routers/company.py
+++ routers/company.py
@@ -19,16 +19,26 @@ def create_company(company: CompanyCreate, db: Session = Depends(get_db)):
 
 @router.get("/",status_code=status.HTTP_200_OK, response_model=list[CompanyResponse])
 def get_all_company(db: Session = Depends(get_db)):
-    pass
+    companies = db.query(Company).all()
+    return companies
 
 @router.get("/{company_id}", status_code=status.HTTP_200_OK, response_model=CompanyResponse)
 def get_company(company_id: int, db: Session = Depends(get_db)):
-    pass
+    company = db.query(Company).filter(Company.id == company_id).first()
+    if not company:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Company not found")
+    return company
  
 @router.put("/{company_id}", status_code=status.HTTP_201_CREATED)
-def update_company(company_id: int, company: CompanyUpdate):
-    companies[company_id] = company
-    return companies
+def update_company(company_id: int, company: CompanyUpdate,db: Session = Depends(get_db)):
+    db_company = db.query(Company).filter(Company.id == company_id).first()
+    if not db_company:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Company not found")
+    for key, value in company.dict().items():
+        setattr(db_company, key, value)
+    db.commit()
+    db.refresh(db_company)
+    return db_company
 
 @router.delete("/{company_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_company(company_id: int):
